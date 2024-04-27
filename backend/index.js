aa28e07e526e850ec022b186fe00cb047ed5a6f3
Martinez-rosa <maura.martinez@alumnos.uneatlantico.es>
2024-04-27 16:35:37 +0200

    Creating HTTP ROUTE

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,17 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(235).send('Welcome to Book Store by Maura Martinez');
+});
+    
 app.listen(PORT, () => {
     console.log(`App is listening to port: ${PORT}`);
 });
+
+
+
+
 
 
