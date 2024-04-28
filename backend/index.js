4637e6dc60d88cb2bc1845c934d339998bf113e9
Martinez-rosa <maura.martinez@alumnos.uneatlantico.es>
2024-04-28 14:34:48 +0200

    Get One Book by id with mongoose

--- backend/index.js
+++ backend/index.js
@@ -52,6 +52,21 @@ app.get('/books', async (request, response) => {
       response.status(500).send({ message: error.message});
     }
 });
+
+// Route for Get One Book from database by id
+app.get('/books/:id', async (request, response) => {
+    try{
+
+       const { id } = request.params;
+
+       const books = await Book.findId(id);
+
+      return response.status(200).json(book);
+    } catch(error){
+      console.log(error,message);   
+      response.status(500).send({ message: error.message});
+    }
+});
     
 
 
